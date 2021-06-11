9dffcabd0b2fe4253642a83eadce0229eaddeec9
Julian <justdotjs@icloud.com>
2021-06-11 13:26:27 +0800

    iOS 15

---
+++ Sources/Loadability/Extensions/View-ErrorAlert.swift
@@ -0,0 +1,20 @@
+import SwiftUI
+
+public extension View {
+    /// Presents an alert to the user if an error occurs.
+    /// - Parameters:
+    ///   - alertContent: Content to display in the alert.
+    func errorAlert(isPresented: Binding<Bool>, error optionalError: Error?, message: ((Error) -> String)?, dismiss: @escaping () -> Void) -> some View {
+        var error: _LocalizedError?
+        if let optionalError = optionalError {
+            error = _LocalizedError(optionalError)
+        }
+        
+        return body
+            .alert(isPresented: isPresented, error: error) { _ in
+                Button("OK", action: dismiss)
+            } message: { error in
+                Text(message?(error) ?? error.userVisibleTitle)
+            }
+    }
+}
